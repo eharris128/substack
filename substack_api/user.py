b48715bb1e4591e374e35b2b53af5794fd1cc799
Evan Harris <echarris@smcm.edu>
2024-10-31 15:52:02 -0400

    Get next page of notes reactions

--- substack_api/user.py
+++ substack_api/user.py
@@ -87,10 +87,24 @@ def get_user_notes(user_id: int):
     """
     endpoint = f"https://substack.com/api/v1/reader/feed/profile/{user_id}"
     r = requests.get(endpoint, headers=HEADERS, timeout=30)
+    next_cursor = r.json()["nextCursor"]
+    print(f"Next cursor: {next_cursor}")
     notes = r.json()["items"]
+    
     # Get notes
-    # filtered_notes = [note["comment"] for note in notes if note["type"] == "comment"]
-
+    all_reactions = []
     # Get note reaction count
     filtered_notes = [note["comment"]["reaction_count"] for note in notes if note["type"] == "comment"]
-    return filtered_notes
+    all_reactions.extend(filtered_notes)
+    endpoint_with_cursor = f"https://substack.com/api/v1/reader/feed/profile/{user_id}?cursor={next_cursor}"
+    next_page_response = requests.get(endpoint_with_cursor, headers=HEADERS, timeout=30)
+
+    # The following cursor
+    next_page_response_cursor = next_page_response.json()["nextCursor"]
+    next_cursor = r.json()["nextCursor"]
+    
+    notes = next_page_response.json()["items"]
+    filtered_notes = [note["comment"]["reaction_count"] for note in notes if note["type"] == "comment"]
+    all_reactions.extend(filtered_notes)
+
+    return all_reactions
